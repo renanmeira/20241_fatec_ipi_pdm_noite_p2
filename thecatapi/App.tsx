c49693422b36c01da78272701e7fc21d6cb230d0
wagner lima <wagner.lima.andrade@alumni.usp.br>
2024-06-09 21:33:53 -0300

    feat:label_buscar_lista_de e botao buscar

--- thecatapi/App.tsx
+++ thecatapi/App.tsx
@@ -1,20 +1,60 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  Button,
+  Pressable,
+  StyleSheet,
+  TextInput,
+  Text,
+  View
+  } from 'react-native';
+  import {
+    useState
+    } from 'react'
 
-export default function App() {
+
+  export default function App() {
+  const [busca, setBusca] = useState('')
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  <View style={styles.container}>
+    <TextInput 
+    style={styles.input}
+    placeholder='Buscar uma lista de...'
+    value={busca}
+    onChangeText={setBusca}/>
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+<Pressable
+style={styles.button}>
+<Text
+style={styles.buttonText}>
+Buscar
+</Text>
+</Pressable>
+  </View>
+
+  
+  );
+  }
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    input: { 
+      width: '80%',
+      borderColor: 'gray',
+      borderWidth: 1,
+      marginBottom: 10,
+    },
+
+    button: {
+      width: '80%',
+      backgroundColor: '#0096F3', //material design blue 500
+      padding: 12,
+      borderRadius: 4
+      },
+      buttonText: {
+      color: 'white',
+      textAlign: 'center'
+      }
+  });
