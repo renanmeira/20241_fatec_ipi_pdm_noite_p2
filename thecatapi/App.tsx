fa41bcd811309f445b7ed837ab755eb7abc9da94
wagner lima <wagner.lima.andrade@alumni.usp.br>
2024-06-10 08:55:54 -0300

    fix: ajuste botão e dependencias

--- thecatapi/App.tsx
+++ thecatapi/App.tsx
@@ -1,60 +1,66 @@
 import {
-  Button,
-  Pressable,
   StyleSheet,
+  View,
   TextInput,
+  Button,
   Text,
-  View
+  Pressable
   } from 'react-native';
+
   import {
     useState
-    } from 'react'
-
+    } from 'react';
 
   export default function App() {
-  const [busca, setBusca] = useState('')
+    const [busca, SetBusca] = useState('')
   return (
   <View style={styles.container}>
-    <TextInput 
+    <TextInput
     style={styles.input}
-    placeholder='Buscar uma lista de...'
-    value={busca}
-    onChangeText={setBusca}/>
-
-<Pressable
-style={styles.button}>
-<Text
-style={styles.buttonText}>
-Buscar
-</Text>
-</Pressable>
+     placeholder='Buscar gatos'
+     value={busca}
+     onChangeText={SetBusca}
+/>
+  <Pressable
+  style={styles.button}>
+  <Text style={styles.buttonText}>
+  Buscar
+  </Text >
+  </Pressable>
   </View>
-
-  
+    
   );
   }
+  
   const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    input: { 
-      width: '80%',
-      borderColor: 'gray',
-      borderWidth: 1,
-      marginBottom: 10,
-    },
+  container: {
+  flex: 1,
+  backgroundColor: '#fff',
+  alignItems: 'center',
+  justifyContent: 'center',
+  },
 
-    button: {
-      width: '80%',
-      backgroundColor: '#0096F3', //material design blue 500
-      padding: 12,
-      borderRadius: 4
-      },
-      buttonText: {
-      color: 'white',
-      textAlign: 'center'
-      }
+  input: {
+    width: '80%',
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 10,
+    padding: 10,
+    textAlign: 'center',
+    borderRadius: 4
+    },
+  
+  button: {
+  width: '80%',
+  backgroundColor: '#0096F3', //material design blue 500
+  padding: 12,
+  borderRadius: 4
+  },
+
+  buttonText: {
+  color: 'white',
+  textAlign: 'center'
+  }
+
   });
+  
