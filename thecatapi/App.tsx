f4b427cd742839d405f78b6f7148de20a7cc8ed3
Renan Meira <renan.meira@gmail.com>
2024-06-11 23:11:02 -0300

    feat: mostra gatinhos em tela

--- thecatapi/App.tsx
+++ thecatapi/App.tsx
@@ -3,15 +3,23 @@ import {
   View,
   TextInput,
   Text,
-  Pressable
+  Pressable,
+  FlatList,
+  Image
 } from 'react-native';
 
 import {
   useState
 } from 'react';
 
+interface Cat {
+  id: string;
+  url: string;
+}
+
 export default function App() {
   const [busca, SetBusca] = useState('')
+  const [imagens, setImagens] = useState<Cat[]>([]);
 
   const buscarGatos = () => {
     fetch(`https://api.thecatapi.com/v1/images/search?limit=${busca}`, {
@@ -19,8 +27,8 @@ export default function App() {
       headers: {'x-api-key': 'live_NlMToAhRoePKfi8lz88338uu7cKIV7MyYoZC4Qraf1ZSmCmmvSWk6V9falQT347f'}
     })
       .then(response => response.json())
-      .then(data => {
-        console.log(data)
+      .then((data: Cat[]) => {
+        setImagens(data);
       })
       .catch(error => {
         console.error(error)
@@ -41,6 +49,14 @@ export default function App() {
           Buscar
         </Text >
       </Pressable>
+      <FlatList
+        style={styles.imageList}
+        data={imagens}
+        renderItem={({ item }) => (
+          <Image source={{ uri: item.url }} style={styles.image} />
+        )}
+        keyExtractor={item => item.id}
+      />
     </View>
   );
 }
@@ -73,6 +89,14 @@ const styles = StyleSheet.create({
   buttonText: {
     color: 'white',
     textAlign: 'center'
+  },
+  imageList: {
+    width: '100%',
+  },
+  image: {
+    width: '100%',
+    height: 200,
+    marginBottom: 10,
   }
 
 });
